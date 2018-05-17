6133f8d4ab3e43157cfa17df1f2ef56ad162b880
diekus <diekus@gmail.com>
2018-05-17 14:08:10 +0100

    dev up

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    deviceDetection();
+    
 });
 
 var wsc_onspeechend = function(){};
--- js/devDet.js
+++ js/devDet.js
@@ -33,7 +33,6 @@ var deviceDetection = function(){
       }
       else{ // spec not implemented
         createCursor();
-        
       }
 }
 
@@ -41,13 +40,13 @@ var deviceDetection = function(){
 var createCursor = function(){
   
   //creates camera
-  var t_cam = document.querySelector('[camera]')
+  var t_cam = document.querySelector('[camera]');
   //creates and attadches cursor 
   var t_cursor = document.createElement('a-entity');
   t_cursor.setAttribute('cursor', 'fuse:true; fuseTimeout:500');
   t_cursor.setAttribute('position', '0 0 -1');
-  t_cursor.setAttribute('geometry', 'primitive: ring; radiusInner: 0.02; radiusOuter: 0.03');
-  t_cursor.setAttribute('material', 'color: black; shader: flat');
+  t_cursor.setAttribute('geometry', 'primitive: ring; radiusInner: 0.006; radiusOuter: 0.007');
+  t_cursor.setAttribute('material', 'color: black; shader: flat; transparent:true; opacity:0.9');
   //t_cursor.setAttribute('raycaster', 'objects:.telesphere');
 
   t_cam.appendChild(t_cursor);
@@ -55,14 +54,20 @@ var createCursor = function(){
 }
 
 var addLaserControls = function(){
-  var t_laserCtrls_L = document.createElement('a-entity');
-  t_laserCtrls_L.setAttribute('laser-controls', 'hand:left');
-  //t_laserCtrls_L.setAttribute('raycaster', 'objects:.click');
-  t_laserCtrls_L.setAttribute('collider-check', '');
-  document.querySelector('a-scene').appendChild(t_laserCtrls_L);
-  var t_laserCtrls_R = document.createElement('a-entity');
-  t_laserCtrls_R.setAttribute('laser-controls', 'hand:right');
-  //t_laserCtrls_R.setAttribute('raycaster', 'objects:.click');
-  t_laserCtrls_R.setAttribute('collider-check', '');
-  document.querySelector('a-scene').appendChild(t_laserCtrls_R);
+  // var t_laserCtrls_L = document.createElement('a-entity');
+  // t_laserCtrls_L.setAttribute('laser-controls', 'hand:left');
+  // //t_laserCtrls_L.setAttribute('raycaster', 'objects:.click');
+  // t_laserCtrls_L.setAttribute('collider-check', '');
+  // document.querySelector('a-scene').appendChild(t_laserCtrls_L);
+  // var t_laserCtrls_R = document.createElement('a-entity');
+  // t_laserCtrls_R.setAttribute('laser-controls', 'hand:right');
+  // //t_laserCtrls_R.setAttribute('raycaster', 'objects:.click');
+  // t_laserCtrls_R.setAttribute('collider-check', '');
+  // document.querySelector('a-scene').appendChild(t_laserCtrls_R);
+  console.log('added laser controls');
 }
+
+//performs device detection once the scene has loaded
+document.addEventListener('DOMContentLoaded', function(){
+  deviceDetection();
+});
